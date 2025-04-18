e4d805c038696e06b93fcb34d077d6bf178f1720
egehelyas <elyas.egeh@gmail.com>
2025-04-18 17:36:14 -0400

    Save all changes including BatchForm component and App updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BatchForm from './components/BatchForm'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 function App() {
   const [count, setCount] = useState(0)
